4dbb80b7e93a0ef432a1d29338dbf942edf6b69b
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-10-22 14:25:07 -0300

    feature: criação da página de calculadora

--- src/pages/Calculadora.tsx
+++ src/pages/Calculadora.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import CalculadoraChurrasco from '../components/CalculadoraChurrasco'
 
 const Calculadora = () => {
+
   return (
-    <div>Calculadora</div>
+    <div>
+      <h1>Calculadora de Churrasco</h1>
+      <CalculadoraChurrasco/>
+    </div>
   )
+
 }
 
 export default Calculadora
